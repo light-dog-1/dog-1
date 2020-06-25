71ece8d1956720bd6ed3cb6e8d8502769acf05d0
light-dog-1 <2651955759@qq.com>
2020-06-25 20:07:06 +0800

    转移新项目过来

---
+++ 安全卫士/mobileSafe/app/src/main/java/com/light/mobileSafe/activity/SettingActivity.java
@@ -0,0 +1,14 @@
+package com.light.mobileSafe.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+    
+}
---
+++ 安全卫士/mobileSafe/app/src/main/java/com/light/mobileSafe/db/domain/BlackNumberInfo.java
@@ -0,0 +1,21 @@
+package com.light.mobileSafe.db.domain;
+
+public class BlackNumberInfo {
+    private String phone;
+    private String mode;
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }}
